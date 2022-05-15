286bc76d64509fb276c75fa59c6641d916ed4bd7
jzhan <kira.ost.08@gmail.com>
2022-05-15 13:53:16 +0700

    Create main.rs

---
+++ Fizz Buzz/main.rs
@@ -0,0 +1,16 @@
+use std::io::Write;
+
+fn main() {
+    for i in 1 .. 37 {
+        print!("{}, ", if i % 3 == 0 && i % 5 == 0 {
+                            "Fizz Buzz".to_string()
+                        } else if i % 3 == 0 {
+                            "Fizz".to_string()
+                        } else if i % 5 == 0 {
+                            "Buzz".to_string()
+                        } else {
+                            i.to_string().clone()
+                        });
+        std::io::stdout().flush().unwrap();
+    }
+}
